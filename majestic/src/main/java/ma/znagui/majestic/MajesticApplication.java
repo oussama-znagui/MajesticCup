514c60b0c4ef99d47b2815c0fdb327999329096b
znagui <znagui.info@gmail.com>
2024-12-06 15:36:26 +0100

    Deploy REST API (Docker)

---
+++ majestic/src/main/java/ma/znagui/majestic/MajesticApplication.java
@@ -0,0 +1,13 @@
+package ma.znagui.majestic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajesticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajesticApplication.class, args);
+    }
+
+}
---
+++ majestic/src/test/java/ma/znagui/majestic/MajesticApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.znagui.majestic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajesticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
